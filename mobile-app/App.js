a12d7fd9038106f89025c19fe10a7176d4a5bd88
Yüce <yuceyuce99@gmail.com>
2021-04-18 23:56:14 +0300

    Update App.js

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
 import { StyleSheet, Text, SafeAreaView, View, Button, Image } from "react-native";
+import RegisterScreen from "./app/screens/RegisterScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 const AUTH_URL = "http://34.78.211.85/shorten";
 
@@ -17,79 +19,28 @@ const requestOptions = {
   body: JSON.stringify( QUERY)
 };
 
+// sendRequest = () => {
+//   console.log("Pressed");
+//   fetch(AUTH_URL, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify( QUERY)
+//   })
+//  .then(() => {
+//     this.setState({
+//        pressed: !(this.state.pressed),
+//        data: "Added",
+//     })
+//  })
+//  .catch((error) => {
+//     console.error(error);
+//  });
+// }
 export default class App extends Component{
-  constructor(props) {
-    super(props)
-    this.state = {
-      pressed: false,
-      data: "",
-       }
-  }
-
-  sendRequest = () => {
-    console.log("Pressed");
-    fetch(AUTH_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify( QUERY)
-    })
-   .then(() => {
-      this.setState({
-         pressed: !(this.state.pressed),
-         data: "Added",
-      })
-   })
-   .catch((error) => {
-      console.error(error);
-   });
-  }
 
   render() {
-    const styles = StyleSheet.create({
-      container: {
-        width: "100%",
-        height: "100%",
-        backgroundColor: "gray",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      logoView:{
-        height: "20%",
-      },
-      logo:{
-        flex: 1,
-        resizeMode: 'contain'
-      },
-      titleView: {
-        height: "50%",
-        alignContent: "center",
-        alignItems: "center"
-      },
-      title: {
-        fontSize: 40,
-        textAlign: "center",
-        alignContent: "center",
-        alignItems: "center",
-        color: "darkblue",
-      },
-      responseView: {
-        width: "90%",
-      },
-    });
     return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.logoView}>
-          <Image style={styles.logo} source={require('./assets/app-logo.png')}/>
-        </View>
-        <View style={styles.titleView}>
-          <Text style={styles.title}>Welcome to Erol Cloud App!</Text>
-        </View>
-        <StatusBar style="auto" />
-        <View style={styles.responseView}>
-          <Button title="Selam" onPress={this.sendRequest} />
-          <Text>{this.state.data}</Text>
-        </View>
-      </SafeAreaView>
+      <WelcomeScreen />
     );
   }
 }
