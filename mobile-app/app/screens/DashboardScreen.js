52c3fe129d8d8175598ff9f16e177bcc7cab764a
Yüce <yuceyuce99@gmail.com>
2021-04-19 22:43:43 +0300

    Add paste from clipboard functionality

--- mobile-app/app/screens/DashboardScreen.js
+++ mobile-app/app/screens/DashboardScreen.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, TextInput, View, TouchableOpacity } from "react-native";
+import Clipboard from "expo-clipboard";
 import { styles } from "../shared/Styles";
 
 export default function Dashboard({ navigation }) {
+  const [copiedText, setCopiedText] = useState("");
+
   const toPostGen = () => {
-    navigation.navigate("PostGen")
-  }
+    navigation.navigate("PostGen");
+  };
+
+  const paste = async () => {
+    Clipboard.setString("hello world");
+    const text = await Clipboard.getStringAsync();
+    console.log(text);
+    setCopiedText(text);
+  };
 
   return (
     <View style={styles.internalBackground}>
@@ -14,8 +24,13 @@ export default function Dashboard({ navigation }) {
           style={styles.inputText}
           placeholder="Original URL"
           placeholderTextColor="#003f5c"
+          value={copiedText}
+          onChangeText={setCopiedText}
         />
       </View>
+      <TouchableOpacity onPress={paste} style={styles.button}>
+        <Text style={styles.text}>Paste</Text>
+      </TouchableOpacity>
       <TouchableOpacity onPress={toPostGen} style={styles.button}>
         <Text style={styles.text}>Shorten</Text>
       </TouchableOpacity>
