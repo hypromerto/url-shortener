df51cbd7926cc8fd3b3034decf681162025ac41d
Yüce <yuceyuce99@gmail.com>
2021-04-19 19:41:42 +0300

    Init post generation screen

---
+++ mobile-app/app/screens/PostGenScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function PostGenScreen({ navigation }) {
+    return (
+        <View>
+            <Text>Generated Successfully</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
