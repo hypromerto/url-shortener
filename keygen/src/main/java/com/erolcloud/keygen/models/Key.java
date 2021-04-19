dd0d46cc59f9c412b38754e6ca01b7bc0716e405
Mert Aslan <aslan-mert99@hotmail.com>
2021-04-20 01:19:58 +0300

    Add key generation

---
+++ keygen/src/main/java/com/erolcloud/keygen/models/Key.java
@@ -0,0 +1,12 @@
+package com.erolcloud.keygen.models;
+
+import lombok.AllArgsConstructor;  
+import lombok.Data;  
+import lombok.NoArgsConstructor;
+
+@Data  
+@AllArgsConstructor  
+@NoArgsConstructor
+public class Key {
+    private String key;
+}
