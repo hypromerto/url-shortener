69d1f34742f9e1efe40ddf92e03dd12950660f97
burakokumus <burakokumus98@gmail.com>
2021-04-23 18:38:45 +0300

    Finish dashboard and postgen

--- mobile-app/app/screens/DashboardScreen.js
+++ mobile-app/app/screens/DashboardScreen.js
@@ -35,30 +35,32 @@ export default function Dashboard({ navigation }) {
   const [customURL, setcustomURL] = useState("");
   const [isSelected, setSelection] = useState(false);
   const [originalURL, setOriginalURL] = useState("");
+  var res;
 
   const toPostGen = () => {
     let QUERY = {
-      originalURL: originalURL,
-      key: "bu ne anlamadım",
-      creator: "gelecek",
-      expirationDate: "tbd", //burada gün sayısı mı vereyim direkt tarih mi
+      "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtZXJ0In0.qmlXAtkOzSS52Zbkhd1NuA3hNrL1cMbLgac7_YT8OzU",
+      "originalURL": originalURL,
+      "expirationDate": "12-04-2022"
     };
-    fetch(AUTH_URL, {
+    fetch("http://34.78.211.85/shorten", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(QUERY),
     })
-      .then((response) => {
-        console.log(response);
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        console.log(data["key"]);
+        navigation.navigate("PostGen", {data: data["key"]});
       })
       .catch((error) => {
         console.error(error);
       });
-    navigation.navigate("PostGen");
   };
 
   const paste = async () => {
-    //Clipboard.setString("hello world");
+    // Clipboard.setString("hello world");
     const text = await Clipboard.getStringAsync();
     setOriginalURL(text);
   };
