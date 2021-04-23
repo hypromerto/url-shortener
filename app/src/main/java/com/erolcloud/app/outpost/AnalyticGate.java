2f2d8675324607cf623322801864610eb5e3b16f
Ata <h.ataatasoy@gmail.com>
2021-04-23 20:50:05 +0300

    Add AnalyticGate on app

---
+++ app/src/main/java/com/erolcloud/app/outpost/AnalyticGate.java
@@ -0,0 +1,26 @@
+package com.erolcloud.app.outpost;
+
+import java.util.HashMap;
+
+public class AnalyticGate {
+    // private static String ANALYTIC_SERVER = "http://35.240.125.16";
+    private static String ANALYTIC_SERVER = "http://localhost:8080";
+
+    public static String addAnalytic(String creator, String link){
+
+        HashMap<String, Object> json = new HashMap<>();
+
+        json.put("username", creator);
+        json.put("link", link);
+
+        HashMap<String, Object> resp = Envoy.post(ANALYTIC_SERVER + "/add-analytic", json);
+
+        if (resp == null){
+            return null;
+        }
+
+        String result = (String) resp.get("result");
+
+        return result;
+    }
+}
