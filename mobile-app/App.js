a4df87cc273d1de87fab823d36656e2a29c2175b
Ata <h.ataatasoy@gmail.com>
2021-04-23 23:33:11 +0300

    update app.js

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navigation from "./app/navigation/ExternalNavigation"
 import { UserProvider } from "./app/shared/UserContext";
+import Toast from "react-native-toast-message";
 
 export default class App extends Component{
 
@@ -8,6 +9,7 @@ export default class App extends Component{
     return (
       <UserProvider>
         <Navigation />
+          <Toast ref={(ref) => Toast.setRef(ref)} />
       </UserProvider>
     );
   }
